a276618a4ecf86862b05009424b78cbf611c4bb7
Huong <huong@Thuy-Nguyen.local>
2025-11-10 10:16:15 +0700

    Cải thiện tốc độ chat

--- data/import_data.py
+++ data/import_data.py
@@ -11,17 +11,17 @@ LOCAL_DB_URL = os.getenv("POSTGRES_URL")
 def get_conn():
     return psycopg2.connect(LOCAL_DB_URL, cursor_factory=RealDictCursor)
 
-def insert_message(user_id, message, reply=None, session_id=None):
+def insert_message(user_id, message, reply=None, session_id=None, time_spent=None):
     try:
         embedding_vector = embedder.embed(message).tolist()
         with get_conn() as conn:
             with conn.cursor() as cur:
                 cur.execute(
                     """
-                    INSERT INTO whoisme.messages (user_id, session_id, message, reply, embedding_vector)
-                    VALUES (%s, %s, %s, %s, %s)
+                    INSERT INTO whoisme.messages (user_id, session_id, message, reply, embedding_vector, time)
+                    VALUES (%s, %s, %s, %s, %s, %s)
                     """,
-                    (str(user_id), session_id, message, reply, embedding_vector),
+                    (str(user_id), session_id, message, reply, embedding_vector, time_spent or 0),
                 )
             conn.commit()
             print(f"Tin nhắn đã được chèn thành công (session_id={session_id})")
